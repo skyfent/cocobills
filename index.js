b0becd5ab273b0ced695ac968486dc7ac445883b
Skyfent <86125680+skyfent@users.noreply.github.com>
2024-10-31 15:55:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 3001;
  console.log('Connection was successful.');
 app.get('/', (req, res) => {
   res.send('Hello, world! Working Well');
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(port, () => {
+  console.log(`Server listening on port port`);
 });
