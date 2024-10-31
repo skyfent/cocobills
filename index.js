3c8bd2c535dca8be79215f4af1682c0f685e1fea
Skyfent <86125680+skyfent@users.noreply.github.com>
2024-10-31 20:49:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 6799;
  console.log('Connection was successful.');
 app.get('/', (req, res) => {
-  res.send('Hello, world! Working Well');
+ // res.send('Hello, world! Working Well');
+ {
+  "message": "Hello, world! Working Well"
+}
 });
 
 app.listen(port, () => {
